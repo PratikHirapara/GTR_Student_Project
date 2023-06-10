ff6878348af6e42307b736b9370b18300c22bd82
Pratik Hirapara <pratikhirpara4@gmail.com>
2023-06-10 18:24:15 +0200

    print_added

--- nas.py
+++ nas.py
@@ -39,6 +39,7 @@ for train, test in kfold.split(inputs, targets):
             
     # Generate a print
     print('----------------------------------------------------------------------')
+    print("____")
     print(f'Training for fold {fold_no} ...')
 
     
